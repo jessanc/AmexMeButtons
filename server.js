e4ef5134c63ebf1bfc705a297c0c067ef221d11e
Jessan <jessan.cherian@gmail.com>
2017-09-25 13:26:51 -0400

    structure

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Promise = require('promise');
-var mongodbUri = 'mongodb://admin:password@ds149144.mlab.com:49144/amexmebuttons';
+//var mongodbUri = 'mongodb://admin:password@ds149144.mlab.com:49144/amexmebuttons';
+var mongodbUri = 'mongodb://localhost/transactionsDB'
 var bodyParser = require('body-Parser');
 var firebase = require('firebase');
+var routes = require('router');
 var port = 8888;
 
 
 var app = express();
-app.listen(port,function(){
+app.use(express.static('public'));
+app.use(routes);
+
+var server =app.listen(port,function(){
 	console.log("app listening on port " + port);
 });
 var options = { useMongoClient : true};  
@@ -16,13 +21,27 @@ var options = { useMongoClient : true};
 mongoose.connect(mongodbUri ,{ useMongoClient: true, promiseLibrary: global.Promise });
 //Define a schema
 var Schema = mongoose.schema;
-var Cat = mongoose.model('Cat', { name: String });
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
+var Transaction = mongoose.model('Transaction', { 
+	name: String 
+
+});
+var sampleTransaction = new Transaction({ 
+	name: 'Zildjian' 
+
+});
+/*sampleTransaction.save(function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('meow');
+    console.log('Trnsaction added');
   }
+});*/
+
+Transaction.find(function(err,transaction){
+	if(err)
+		console.log(err);
+	console.log(transaction);
 });
+
+
 
