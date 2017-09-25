89d2e07f8d11c3e57c33774ef3c30bb6e48a859a
Jessan <jessan.cherian@gmail.com>
2017-09-25 18:41:27 -0400

    firebase working

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var router = express.Router();
 var mongoose = require('mongoose');
+var router = express.Router();
+var bodyParser = require('body-Parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -15,7 +18,9 @@ router.get('/', function(req, res) {
 });
 
 router.get('/transactions',function(req,res){
-	mongoose.model('Transaction').find(function(err, transaction) {
+	var transactionModel = mongoose.model('Transaction');
+  console.log(req.body);
+	transactionModel.find(function(err, transaction) {
             if (err)
                 res.send(err);
 
@@ -23,6 +28,18 @@ router.get('/transactions',function(req,res){
         });
 });
 
+router.post('/transaction',function(req,res){
+	var transactionModel = mongoose.model('Transaction');
+  var transaction = new transactionModel();
+  //console.log(req.body);
+  //add all fields of transaction from trnsaction
+  transaction.Merchant = req.body.Merchant;
+  transaction.save(function(err){
+    if(err)
+      console.log(err);
+    res.json({message : 'Transaction added'});
+  })
+})
 
 // Define the about route
 router.get('/about', function(req, res) {
--- server.js
+++ server.js
@@ -4,24 +4,53 @@ var Promise = require('promise');
 //var mongodbUri = 'mongodb://admin:password@ds149144.mlab.com:49144/amexmebuttons';
 var mongodbUri = 'mongodb://localhost/transactionsDB'
 var bodyParser = require('body-Parser');
-//var firebase = require('firebase');
 var routes = require('./routes/routes.js');
-var port = 8888;
-
-
-var app = express();
+var app = module.exports = express();
+var admin = require('firebase-admin');
+var serviceAccount = require("./key.json");
 //app.use(express.static('public'));
 app.use(routes);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var server =app.listen(port,function(){
+var ip = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8765;
+var server =app.listen(port,ip,function(){
 	console.log("app listening on port " + port);
 });
-var options = { useMongoClient : true};  
+var options = { useMongoClient : true};
 mongoose.connect(mongodbUri ,{ useMongoClient: true, promiseLibrary: global.Promise });
-//Define a schema
+//firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-mebuttons.firebaseio.com"
+});
 
-var Transaction = mongoose.model('Transaction', { 
+var topic = "test";
+
+// See the "Defining the message payload" section below for details
+// on how to define a message payload.
+var payload = {
+  data: {
+    title: "blahhasdadas",
+    body: "asd"
+  }
+};
+var foptions = {
+	click_action: "android.intent.action.MAIN"
+}
+// Send a message to devices subscribed to the provided topic.
+admin.messaging().sendToTopic(topic, payload,foptions)
+  .then(function(response) {
+    // See the MessagingTopicResponse reference documentation for the
+    // contents of response.
+    console.log("Successfully sent message:", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+  });
+
+
+var Transaction = mongoose.model('Transaction', {
 	time : { type : Date, default: Date.now },
 	Merchant : String,
 	Description: String,
@@ -32,7 +61,7 @@ var Transaction = mongoose.model('Transaction', {
 });
 var date = new Date(); //2017-04-25T06:23:36.510Z
 date.toLocaleTimeString();  //'11:53:36 AM'
-var sampleTransaction = new Transaction({ 
+var sampleTransaction = new Transaction({
 	time : date,
 	Merchant: 'Paypal',
 	Description: 'Gift',
@@ -41,19 +70,9 @@ var sampleTransaction = new Transaction({
 	Flag : false,
 	TagCategory : 'none'
 });
-/*sampleTransaction.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Trnsaction added');
-  }
-});*/
 
 Transaction.find(function(err,transaction){
 	if(err)
 		console.log(err);
 	console.log(transaction);
 });
-
-
-
