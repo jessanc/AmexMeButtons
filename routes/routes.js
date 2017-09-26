b1e9db5bc3cb573b6b8edf7f9a980b85e2fa777f
Jessan <jessan.cherian@gmail.com>
2017-09-26 09:44:36 -0400

    server working with routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
-var bodyParser = require('body-Parser');
+var bodyParser = require('body-parser');
+var admin = require('firebase-admin');
+var serviceAccount = require("../key.json");
+
+//firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-mebuttons.firebaseio.com"
+});
+var topic = "test";
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 //Middle ware that is specific to this router
@@ -32,15 +41,69 @@ router.post('/transaction',function(req,res){
 	var transactionModel = mongoose.model('Transaction');
   var transaction = new transactionModel();
   //console.log(req.body);
-  //add all fields of transaction from trnsaction
   transaction.Merchant = req.body.Merchant;
-  transaction.save(function(err){
+  transaction.Description = req.body.Description;
+  transaction.Amount = req.body.Amount;
+  transaction.Status = req.body.Status;
+  transaction.save(function(err,saved){
     if(err)
       console.log(err);
     res.json({message : 'Transaction added'});
+    console.log(saved.id);
+    //sending firebase
+    var payload = {
+      data: {
+        Merchant : saved.Merchant,
+        Amount: saved.Amount.toString(),
+        ID: saved.id
+      }
+    };
+    // Send a message to devices subscribed to the provided topic.
+    admin.messaging().sendToTopic(topic, payload)
+      .then(function(response) {
+        // See the MessagingTopicResponse reference documentation for the
+        // contents of response.
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function(error) {
+        console.log("Error sending message:", error);
+      });
   })
 })
+router.post('/tagMe',function(req,res){
+  var transactionModel = mongoose.model('Transaction');
+  console.log(req.body);
+  var id = req.body.ID;
+  var objID = mongoose.Types.ObjectId(id);
+  console.log("ID received : " + objID);
+  //   transactionModel.findById(objID, function (err, trans){
+  //     if(err)
+  //     console.log(err);
+  //     console.log("Transaction found with id : " + trans);
+  // });
+  transactionModel.update({_id: id},{Flag : true },
+     {multi:true},
+       function(err, numberAffected){
+         if(err)
+         console.log(err);
+         console.log(numberAffected);
+       });
 
+  res.sendStatus(200);
+});
+router.post('/callMe',function(req,res){
+  var transactionModel = mongoose.model('Transaction');
+  console.log(req.body);
+  var id = req.body.ID;
+  var objID = mongoose.Types.ObjectId(id);
+  console.log("ID received for callme: " + objID);
+    transactionModel.findById(objID, function (err, trans){
+      if(err)
+      console.log(err);
+      console.log("Transaction found with id(callme) : " + trans);
+  });
+  res.sendStatus(200);
+})
 // Define the about route
 router.get('/about', function(req, res) {
   res.send('About us');
--- server.js
+++ server.js
@@ -2,53 +2,20 @@ var express = require('express');
 var mongoose = require('mongoose');
 var Promise = require('promise');
 //var mongodbUri = 'mongodb://admin:password@ds149144.mlab.com:49144/amexmebuttons';
-var mongodbUri = 'mongodb://localhost/transactionsDB'
-var bodyParser = require('body-Parser');
+var mongodbUri = 'mongodb://admin:password@ds149144.mlab.com:49144/amexmebuttons';
+var bodyParser = require('body-parser');
 var routes = require('./routes/routes.js');
 var app = module.exports = express();
-var admin = require('firebase-admin');
-var serviceAccount = require("./key.json");
+
 //app.use(express.static('public'));
 app.use(routes);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var ip = process.env.OPENSHIFT_NODEJS_IP;
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8765;
-var server =app.listen(port,ip,function(){
+var port = 1234;
+var server = app.listen(port,function(){
 	console.log("app listening on port " + port);
 });
-var options = { useMongoClient : true};
 mongoose.connect(mongodbUri ,{ useMongoClient: true, promiseLibrary: global.Promise });
-//firebase
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-mebuttons.firebaseio.com"
-});
-
-var topic = "test";
-
-// See the "Defining the message payload" section below for details
-// on how to define a message payload.
-var payload = {
-  data: {
-    title: "blahhasdadas",
-    body: "asd"
-  }
-};
-var foptions = {
-	click_action: "android.intent.action.MAIN"
-}
-// Send a message to devices subscribed to the provided topic.
-admin.messaging().sendToTopic(topic, payload,foptions)
-  .then(function(response) {
-    // See the MessagingTopicResponse reference documentation for the
-    // contents of response.
-    console.log("Successfully sent message:", response);
-  })
-  .catch(function(error) {
-    console.log("Error sending message:", error);
-  });
-
 
 var Transaction = mongoose.model('Transaction', {
 	time : { type : Date, default: Date.now },
@@ -56,21 +23,12 @@ var Transaction = mongoose.model('Transaction', {
 	Description: String,
 	Amount: Number,
 	Status: String,
-	Flag : Boolean,
-	TagCategory : String
+	Flag : {type: Boolean, default: false},
+	TagCategory : String,
+	CallBack : {type: Boolean, default: false}
 });
 var date = new Date(); //2017-04-25T06:23:36.510Z
 date.toLocaleTimeString();  //'11:53:36 AM'
-var sampleTransaction = new Transaction({
-	time : date,
-	Merchant: 'Paypal',
-	Description: 'Gift',
-	Amount: '25.50',
-	Status: 'Pending',
-	Flag : false,
-	TagCategory : 'none'
-});
-
 Transaction.find(function(err,transaction){
 	if(err)
 		console.log(err);
